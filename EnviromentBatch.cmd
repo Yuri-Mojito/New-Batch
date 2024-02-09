0f601a88ba21eab899eb15a1ca55b6235cf54e24
Yuri Mojito <80856836+Yuri-Mojito@users.noreply.github.com>
2024-02-09 08:13:31 -0700

    new commit
    new add commit with the new file because it´s amazing

---
+++ EnviromentBatch.cmd
@@ -0,0 +1,13 @@
+@echo off 
+title Inicialization of VEnviroment --coded by YURI
+Rem This code to start VEnviroment Python
+Rem or :: to comment
+color 0A 
+
+Rem Path with title
+call "./.env/Scripts/Activate"
+cd C:\Documents\Plataforma Asesorias\Asesorias COBE\
+Python manage.py
+
+REM Keep the window open
+pause
